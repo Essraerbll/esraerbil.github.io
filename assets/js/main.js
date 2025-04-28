cdfcac4f55a75af2d41ddb43d2a0e99ebd60ebce
esrae <esraerbill652@gmail.com>
2025-04-28 21:41:50 +0300

    .

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-/* == MENU SHOW == */
+
 const showMenu = (toggleId, navId) => {
     const toggle = document.getElementById(toggleId),
           nav = document.getElementById(navId);
@@ -11,7 +11,7 @@ const showMenu = (toggleId, navId) => {
 };
 showMenu('nav-toggle', 'nav-menu');
 
-/* ===== REMOVE MENU MOBILE AFTER CLICK ===== */
+
 const navLink = document.querySelectorAll('.nav__link');
 
 function linkAction() {
@@ -20,7 +20,6 @@ function linkAction() {
 }
 navLink.forEach(n => n.addEventListener('click', linkAction));
 
-/* ===== SCROLL SECTIONS ACTIVE LINK ===== */
 const sections = document.querySelectorAll('section[id]');
 
 function scrollActive() {
@@ -41,7 +40,7 @@ function scrollActive() {
 }
 window.addEventListener('scroll', scrollActive);
 
-/* ===== SCROLL REVEAL ANIMATION ===== */
+
 const sr = ScrollReveal({
     origin: 'top',
     distance: '50px',
